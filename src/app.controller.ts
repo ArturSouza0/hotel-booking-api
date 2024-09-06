deeb781ed1778e8dd746cb4c1927089e541cc539
Artur Souza <artur.souza955@gmail.com>
2024-09-05 21:33:11 -0300

    implementação metodos e rotas referentes ao dto cargo.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { CargoBody } from './dtos/criar-cargo';
 import { CargoRepository } from './repositories/cargo-repository';
 
 @Controller('app')
 export class AppController {
-  constructor(private cargoRepository: CargoRepository) {}
 
+  constructor(private cargoRepository: CargoRepository) { }
+ 
+  @Get('cargos')
+  async getAllCargos() {
+    return this.cargoRepository.findAll();
+  }
+  
+  @Get('cargo/:id')
+  async getCargoById(@Param('id', ParseIntPipe) id: number) {
+    const cargo = await this.cargoRepository.findById(id);
+    if (!cargo) {
+      throw new Error('Cargo não encontrado');
+    }
+    return cargo;
+  }
+  
   @Post('cargo')
-  async getCargo(@Body() body: CargoBody) {
+  async postCargo(@Body() body: CargoBody) {
     const { descricao } = body;
     await this.cargoRepository.create(descricao);
+    return {message:'Cargo cadastrado com sucesso!' }
+  }
+
+  @Put('cargo/:id')
+  async updateCargo(@Param('id', ParseIntPipe) id: number, @Body() body: CargoBody) {
+    const { descricao } = body;
+    await this.cargoRepository.update(id, descricao);
+    return { message: 'Cargo atualizado com sucesso!' };
+  }
+
+  @Delete('cargo/:id')
+  async deleteCargo(@Param('id', ParseIntPipe) id: number) {
+    await this.cargoRepository.delete(id);
+    return { message: 'Cargo deletado com sucesso!' };
   }
 }
--- src/repositories/prisma/prisma-cargo-repository.ts
+++ src/repositories/prisma/prisma-cargo-repository.ts
@@ -1,16 +1,41 @@
 import { PrismaService } from 'src/database/prisma.service';
 import { CargoRepository } from '../cargo-repository';
 import { Injectable } from '@nestjs/common';
+import { CargoBody } from 'src/dtos/criar-cargo';
 
 @Injectable()
 export class PrismaCargoRepository implements CargoRepository {
-  constructor(private prisma: PrismaService) {}
+  
+  constructor(private prisma: PrismaService) { }
 
   async create(descricao: string): Promise<void> {
     await this.prisma.cargo.create({
       data: {
         descricao,
       },
+    });
+  } 
+
+  async findAll(): Promise<CargoBody[]> {
+    return this.prisma.cargo.findMany();
+  }
+
+  async findById(id:number): Promise<CargoBody | null> {
+    return this.prisma.cargo.findUnique({
+      where: {id},
+    })
+  }
+
+  async update(id: number, descricao: string): Promise<void> {
+      await this.prisma.cargo.update({
+        where: {id},
+        data: {descricao}
+      });
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.prisma.cargo.delete({
+      where: { id },
     });
   }
 }
